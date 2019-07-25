a71411df1801f58d66d3a037608f057c1dc48af8
AHijazi11 <ahmad@Ahmads-MacBook-Pro.local>
2019-07-24 20:26:22 -0400

    QR CODE IS WORKING

--- screens/QRScanner.js
+++ screens/QRScanner.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 import {
   Alert,
   Linking,
@@ -9,13 +10,15 @@ import {
   StatusBar,
   StyleSheet,
   TouchableOpacity
-} from 'react-native';
-import { BarCodeScanner, Permissions } from 'expo';
+} from "react-native";
+import { BarCodeScanner, Permissions } from "expo";
 
 export default class QRScanner extends Component {
   state = {
     hasCameraPermission: null,
-    lastScannedUrl: null
+    lastScannedUrl: null,
+    hasError: false,
+    isLoading: true
   };
 
   componentDidMount() {
@@ -25,15 +28,54 @@ export default class QRScanner extends Component {
   _requestCameraPermission = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({
-      hasCameraPermission: status === 'granted'
+      hasCameraPermission: status === "granted"
     });
   };
 
   _handleBarCodeRead = result => {
     if (result.data !== this.state.lastScannedUrl) {
       LayoutAnimation.spring();
-      this.setState({ lastScannedUrl: result.data });
+      this.setState({ lastScannedUrl: result.data }, () => {
+        this.GetJSONFromGoogleSheet();
+      });
     }
+  };
+
+  parseSheetId = () => {
+    return this.state.lastScannedUrl.slice(
+      this.state.lastScannedUrl.lastIndexOf("spreadsheets/d/") + 15,
+      this.state.lastScannedUrl.lastIndexOf("/")
+    );
+  };
+
+  GetJSONFromGoogleSheet = () => {
+    axios
+      .get(
+        `https://spreadsheets.google.com/feeds/cells/` +
+          this.parseSheetId() +
+          `/1/public/full?alt=json`
+      )
+      .then(data => {
+        let ArrayFromSheet = [];
+        let Deck = { Title: "", flashcards: [] };
+        data.data.feed.entry.map(i => ArrayFromSheet.push(i.gs$cell));
+
+        ArrayFromSheet.map((cell, index) => {
+          if (cell.inputValue == "Title") {
+            Deck.Title = ArrayFromSheet[index + 1].inputValue;
+          } else if (cell.row >= 4 && cell.col == 1) {
+            Deck.flashcards.push({
+              front: cell.inputValue,
+              back: ArrayFromSheet[index + 1].inputValue
+            });
+          }
+        });
+
+        this.props.navigation.navigate("CreateDeck", {
+          flashcards: Deck.flashcards
+        });
+      })
+      .catch(() => this.setState({ hasError: true, isLoading: false }));
   };
 
   render() {
@@ -42,36 +84,32 @@ export default class QRScanner extends Component {
         {this.state.hasCameraPermission === null ? (
           <Text>Requesting for camera permission</Text>
         ) : this.state.hasCameraPermission === false ? (
-          <Text style={{ color: '#fff' }}>
+          <Text style={{ color: "#fff" }}>
             Camera permission is not granted
           </Text>
         ) : (
           <BarCodeScanner
             onBarCodeRead={this._handleBarCodeRead}
             style={{
-              height: Dimensions.get('window').height,
-              width: Dimensions.get('window').width
+              height: Dimensions.get("window").height,
+              width: Dimensions.get("window").width
             }}
           />
         )}
-
-        {this._maybeRenderUrl()}
-
-        <StatusBar hidden />
       </View>
     );
   }
 
   _handlePressUrl = () => {
     Alert.alert(
-      'Open this URL?',
+      "Open this URL?",
       this.state.lastScannedUrl,
       [
         {
-          text: 'Yes',
+          text: "Yes",
           onPress: () => Linking.openURL(this.state.lastScannedUrl)
         },
-        { text: 'No', onPress: () => {} }
+        { text: "No", onPress: () => {} }
       ],
       { cancellable: false }
     );
@@ -90,7 +128,7 @@ export default class QRScanner extends Component {
       <View style={styles.bottomBar}>
         <TouchableOpacity style={styles.url} onPress={this._handlePressUrl}>
           <Text numberOfLines={1} style={styles.urlText}>
-            {this.state.lastScannedUrl}
+            test
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
@@ -107,33 +145,33 @@ export default class QRScanner extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#000'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000"
   },
   bottomBar: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: "rgba(0,0,0,0.5)",
     padding: 15,
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   url: {
     flex: 1
   },
   urlText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 20
   },
   cancelButton: {
     marginLeft: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   cancelButtonText: {
-    color: 'rgba(255,255,255,0.8)',
+    color: "rgba(255,255,255,0.8)",
     fontSize: 18
   }
 });
